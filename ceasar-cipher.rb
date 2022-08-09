931e70a634c16bebca967b40c4d70c86f27b22e4
tomaszwasik31 <80346266+tomaszwasik31@users.noreply.github.com>
2022-08-09 13:33:59 +0100

    Add files via upload

---
+++ ceasar-cipher.rb
@@ -0,0 +1,22 @@
+def caesar_cipher(string,key=0)
+
+result=""
+  string.each_char do |e|
+
+    if  e.ord.between?(65, 90) || e.ord.between?(97, 122)
+        new_e = e.ord+key
+        new_e -= 26 if (new_e > 90 && e.ord <= 90 )  || (e.ord>=97&& new_e>122)
+            result += new_e.chr
+            
+            else
+            result+= e
+        end
+    end
+     result
+end
+
+
+
+
+ puts (caesar_cipher("What a string!", 5))
+
